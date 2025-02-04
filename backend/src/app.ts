fcf2bdb2a37188344afa5098824de3a6a0adc184
naimuddin94 <developernaimuddin@gmail.com>
2025-02-04 21:55:50 +0600

    initial backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,41 @@
+/*
+ * Title: Jotter
+ * Description: It's a backend application for Storage Management System.
+ * Author: Md Naim Uddin
+ * Date: 04/02/2025
+ *
+ */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { globalErrorHandler, notFound } from './app/utils';
+
+const app: Application = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+);
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Testing
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send({ message: 'Jotter server is running :(' });
+});
+
+//global error handler
+app.use(globalErrorHandler as unknown as express.ErrorRequestHandler);
+
+//handle not found
+app.use(notFound);
+
+export default app;
---
+++ backend/src/app/utils/index.ts
@@ -0,0 +1,24 @@
+import AppError from './AppError';
+import AppResponse from './AppResponse';
+import asyncHandler from './asyncHandler';
+import fileUploadOnCloudinary from './fileUploadOnCloudinary';
+import globalErrorHandler from './globalErrorHandler';
+import notFound from './notFound';
+
+// JWT configuration
+const options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  maxAge: 365 * 24 * 60 * 60 * 1000,
+};
+
+export {
+  AppError,
+  AppResponse,
+  asyncHandler,
+  fileUploadOnCloudinary,
+  globalErrorHandler,
+  notFound,
+  options,
+};
